c4e478813a9b519a78a12fd369eb5fd82975f03c
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-06-07 23:20:34 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { MissingParamError } from '../error/missing-param-error'
 import { BadRequest } from '../helper/http-helper'
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
-    if (!httpRequest.body.name) {
-      return BadRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return BadRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return BadRequest(new MissingParamError(field))
+      }
     }
   }
 }
