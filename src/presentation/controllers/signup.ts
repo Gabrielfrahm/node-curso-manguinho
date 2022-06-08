c90aad2066459e1a4e3dff7964cc0a71398e5177
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-06-07 23:31:22 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../error/missing-param-error'
 import { BadRequest } from '../helper/http-helper'
-export class SingUpController {
+import { Controller } from '../protocols/controller'
+export class SingUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
