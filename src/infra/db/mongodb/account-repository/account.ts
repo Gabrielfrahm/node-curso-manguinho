2693098b39f4fd23500bf7a6f4f158e455e0d937
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-06-14 20:43:24 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { mongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = mongoHelper.getCollection('accounts')
+    const { insertedId } = await accountCollection.insertOne(account)
+    const accountFind = await accountCollection.findOne<AccountModel>()
+    return Object.assign({}, accountFind, { id: insertedId })
+  }
+}
