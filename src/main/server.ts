9652ed97090749d9202670c5f7584a8e77ad889c
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-06-17 08:58:14 -0300

    feat: add ExpressRouteAdapter

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,5 +3,5 @@ import env from './config/env'
 
 mongoHelper.connect(env.mongoUrl).then(async () => {
   const app = (await import('./config/app')).default
-  app.listen(env.port, () => console.log(`🚀 Server running at http://127.0.0.1:${env.port}`))
+  app.listen(env.port, () => console.log(`🚀 Server running at http://localhost:${env.port}`))
 }).catch(console.error)
