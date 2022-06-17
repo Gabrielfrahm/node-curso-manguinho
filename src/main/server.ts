7c508d75568b734524d9e8b4d226e1fce733fe7c
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-06-17 07:53:48 -0300

    feat: connect to mongodb before app listem

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,7 @@
-import app from './config/app'
+import { mongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(3333, () => console.log('🚀 Server running at http://127.0.0.1:3333'))
+mongoHelper.connect(env.mongoUrl).then(async () => {
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => console.log(`🚀 Server running at http://127.0.0.1:${env.port}`))
+}).catch(console.error)
