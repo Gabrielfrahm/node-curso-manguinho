7d6fd7782763659682a49e83ef5003a47d086d29
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-08-30 08:06:16 -0300

    test: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -118,7 +118,7 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
-  it('Should returns 401 if invalid credentials are provided', async () => {
+  it('Should returns 200 if valid credentials are provided', async () => {
     const { sut } = makeSut()
 
     const httpResponse = await sut.handle(makeHttpRequestFake())
