2cd27b5ceb361d362abb2ee55a26777a3d6df421
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-09-02 08:12:09 -0300

    test: ensure dbAuthentication Throws if LoadAccountByEmailRepository throws

--- src/data/usecases/authentication/db-autentication.spec.ts
+++ src/data/usecases/authentication/db-autentication.spec.ts
@@ -45,4 +45,10 @@ describe('DbAuthentication useCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
+  it('Should throw if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(Promise.reject(new Error()))
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
 })
