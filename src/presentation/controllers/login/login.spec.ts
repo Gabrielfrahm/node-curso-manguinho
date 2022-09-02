1c1b9037780ba78c50266cdeda572c51ed139344
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-09-02 07:38:10 -0300

    reafctor: create an interface model authentication

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, Authentication, Validation } from './login-protocols'
+import { HttpRequest, Authentication, Validation, AuthenticationModel } from './login-protocols'
 import { MissingParamError } from '../../error'
 import { BadRequest, serverError, unauthorized, ok } from '../../helper/http/http-helper'
 import { LoginController } from './login'
@@ -31,7 +31,7 @@ const makeFakeRequest = (): HttpRequest => ({
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth (email: string, password: string): Promise<string> {
+    async auth (authentication: AuthenticationModel): Promise<string> {
       return 'any_token'
     }
   }
@@ -62,7 +62,7 @@ describe('Login Controller', () => {
     const authSpy = jest.spyOn(authenticationStub, 'auth')
 
     await sut.handle(makeHttpRequestFake())
-    expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password')
+    expect(authSpy).toHaveBeenCalledWith({ email: 'any_email@mail.com', password: 'any_password' })
   })
 
   it('Should returns 401 if invalid credentials are provided', async () => {
